5c4f31367008ef58106f68c5e3f67c892a5319f2
Ilya Yanok <ilya.yanok@gmail.com>
2015-09-15 21:05:44 +0200

    cb-agda: try to use agda-mode from Agda
    doesn't really work yet. Have to restart Agda all the time.

--- cb-agda/extensions.el
+++ cb-agda/extensions.el
@@ -27,9 +27,18 @@
     (add-to-list 'aggressive-indent-excluded-modes 'agda2-mode)))
 
 (defun cb-agda/init-agda-mode ()
-  (use-package agda2
+  (unless (executable-find "agda-mode")
+    (spacemacs-buffer/warning
+     (concat "Agda not detected, be sure that Agda binaries are "
+             "available in your PATH or check the installation "
+             "instructions in the README file.")))
+  (use-package agda2-mode
+    :if (executable-find "agda-mode")
+    :defer t
     :init
     (progn
+      (load-file (let ((coding-system-for-read 'utf-8))
+                   (shell-command-to-string "agda-mode locate")))
       (defun cb-agda/configure-agda-mode-hooks ()
         (add-hook 'after-save-hook 'agda2-load nil t)
         (add-hook 'before-save-hook 'agda/rewrite-symbols-in-buffer nil t)
